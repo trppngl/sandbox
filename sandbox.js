3bee89756817e4f9cbffefd2e149e5b4a4529d97
trppngl <jonathanconradmiller@gmail.com>
2017-12-06 02:34:24 -0500

    Merged function getLineRectsFromEls() working well

--- sandbox.js
+++ sandbox.js
@@ -8,9 +8,24 @@ var fragment = document.createDocumentFragment();
 
 var extensionWidth = 8;
 
+// ease-in-out (.42,0,.58,1) 30 frames
+var easingMultipliers = [0.00000, 0.00213, 0.00865, 0.01972, 0.03551, 0.05613, 0.08166, 0.11211, 0.14741, 0.18740, 0.23177, 0.28013, 0.33188, 0.38635, 0.44269, 0.50000, 0.55731, 0.61365, 0.66812, 0.71987, 0.76823, 0.81260, 0.85259, 0.88789, 0.91834, 0.94387, 0.96449, 0.98028, 0.99135, 0.99787, 1.00000]
+
+var totalFrames = easingMultipliers.length - 1;
+var currentFrame = 0;
+
+var currentStart;
+var currentEnd;
+
+var initialStart;
+var initialEnd;
+
+var targetStart;
+var targetEnd;
+
 //
 
-function getRectsFromEls(els) {
+function getLineRectsFromEls(els) {
   
   var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
   var scrollLeft = document.documentElement.scrollLeft || document.body.scrollLeft;
@@ -19,42 +34,48 @@ function getRectsFromEls(els) {
   var columnTop = columnRect.top;
   var columnLeft = columnRect.left;
   
-  var rects = [];
-  els = [].concat(els || [])
-  
-  for (var i = 0; i < els.length; i++) {
-    theseRects = els[i].getClientRects();
-    for (var j = 0; j < theseRects.length; j++) {
-      rects.push({
-        top: theseRects[j].top += scrollTop - columnTop,
-        left: theseRects[j].left += scrollLeft - columnLeft,
-        width: theseRects[j].width
-      });
-    }
-  }
-  
-  return rects;
-}
-
-function getLineRectsFromRects(rects) {
+  var theseRects;
   
   var lineRects = [];
-
-  for (var i = 0; i < rects.length; i++) {
-    currentLineRect = lineRects[lineRects.length - 1];
-    if (!currentLineRect || currentLineRect.top !== rects[i].top) {
-      lineRects.push({
-        top: rects[i].top,
-        left: rects[i].left,
-        width: rects[i].width
-      });
-    } else {
-      currentLineRect.width = rects[i].left + rects[i].width;
+  
+  var top;
+  var left;
+  var width;
+  
+  els = [].concat(els || []); // var els?
+  
+  for (var i = 0; i < els.length; i++) {
+    
+    theseRects = els[i].getClientRects();
+    
+    for (var j = 0; j < theseRects.length; j++) {
+      
+      top = theseRects[j].top + scrollTop - columnTop;
+      left = theseRects[j].left + scrollLeft - columnLeft;
+      width = theseRects[j].width;
+      
+      currentLineRect = lineRects[lineRects.length - 1];
+      
+      if (!currentLineRect || currentLineRect.top !== top) {
+        
+        lineRects.push({
+          top: top,
+          left: left,
+          width: width
+        });
+        
+      } else {
+        
+        currentLineRect.width = left + width;
+        
+      }
     }
   }
+  
+  for (var k = 0; k < lineRects.length - 1; k++) {
 
-  for (var i = 0; i < lineRects.length - 1; i++) {
-    lineRects[i].width += extensionWidth;
+    lineRects[k].width += extensionWidth;
+
   }
   
   return lineRects;
@@ -94,16 +115,35 @@ function makeHighlightBoxes(rects, startOffset, endOffset) {
   highlightPane.appendChild(fragment);
 }
 
-//
+/*
 
-function cachePrevDist(rects) {
+function getLineRectsFromRects(rects) {
   
-  rects[0].prevDist = 0;
-  
-  for (var i = 1; i < rects.length; i++) {
-    rects[i].prevDist = rects[i - 1].prevDist + rects[i - 1].width;
+  var lineRects = [];
+
+  for (var i = 0; i < rects.length; i++) {
+    currentLineRect = lineRects[lineRects.length - 1];
+    if (!currentLineRect || currentLineRect.top !== rects[i].top) {
+      lineRects.push({
+        top: rects[i].top,
+        left: rects[i].left,
+        width: rects[i].width
+      });
+    } else {
+      currentLineRect.width = rects[i].left + rects[i].width;
+    }
   }
+
+  for (var i = 0; i < lineRects.length - 1; i++) {
+    lineRects[i].width += extensionWidth;
+  }
+  
+  return lineRects;
 }
+
+*/
+
+//
 
 /* function getLineFromPt(line, pt) {
 
@@ -121,7 +161,7 @@ function cachePrevDist(rects) {
   }
   
   return line;
-} */
+}
 
 // Below iterates up from 0 and is probably temporary; above iterates up or down from current line and is propably better but needs (forward) condition replaced
 
@@ -148,13 +188,8 @@ function abcd(startPt, endPt) {
   // startLine = getLineFromPt(startLine, startPt);
   // endLine = getLineFromPt(endLine, endPt - 1);
   
-  var t0 = performance.now();
-  
   startLine = getLineFromPt(startPt);
   endLine = getLineFromPt(endPt - 1);
-  
-  var t1 = performance.now();
-  console.log((t1 - t0).toFixed(4), 'milliseconds');
   
   startOffset = startPt - visLineRects[startLine].prevDist;
   endOffset = endPt - visLineRects[endLine].prevDist;
@@ -174,6 +209,69 @@ function abcd(startPt, endPt) {
   
 }
 
-var visLineRects = getLineRectsFromRects(getRectsFromEls(segs));
+// Adapted from trippingly.js
 
-cachePrevDist(visLineRects);
+function startSeg(targetIndex) {
+  currentFrame = 1;
+  prepAnimation(targetIndex);
+  animating = true;
+}
+
+function prepAnimation(targetIndex) {
+  
+  var targetSeg = segs[targetIndex];
+  var targetSegRect = targetSeg.getBoundingClientRect();
+  
+  targetStart = target
+  targetEnd = 
+  
+  initialStart = currentStart // ?
+  initialEnd = currentEnd // ?
+  
+  //
+  
+  startTop = highlight.offsetTop;
+  startHt = highlight.clientHeight;
+  
+  var seg = segs[currentIndex];
+  endTop = seg.offsetTop;
+  endHt = seg.clientHeight;
+}
+
+function animate() {
+    
+  currentStart = Math.round(ease(initialStart, targetStart));
+  currentEnd = Math.round(ease(initialEnd, targetEnd));
+
+  abcd(currentStart, currentEnd);
+
+  if (currentFrame < totalFrames) {
+    currentFrame += 1;
+    requestAnimationFrame(animate);
+  }
+}
+
+function ease(startValue, endValue) {
+  return (endValue - startValue) * easingMultipliers[currentFrame] + startValue;
+}
+
+function cachePrevDist(rects) {
+  
+  rects[0].prevDist = 0;
+  
+  for (var i = 1; i < rects.length; i++) {
+    rects[i].prevDist = rects[i - 1].prevDist + rects[i - 1].width;
+  }
+}
+
+*/
+
+//
+
+var t0 = performance.now();
+
+var visLineRects = getLineRectsFromEls(segs);
+
+var t1 = performance.now();
+console.log((t1 - t0).toFixed(4), 'milliseconds');
+
