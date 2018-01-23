b8460285aed42eeed58d1e82fb6acb2003434795
trppngl <jonathanconradmiller@gmail.com>
2018-01-23 03:25:53 -0500

    Tweaked toggle card functionality to handle cards with no next sentence (last on page), and cleaned up JS a little

--- bastille.js
+++ bastille.js
@@ -1,13 +1,15 @@
 'use strict';
 
-var column = document.getElementById('column');
+var columnEl = document.getElementById('column');
+
+var currentCardIndex;
 
 var t0 = performance.now();
 
-var sentences = [];
-var firstSegs = [];
-var notecards = [];
-var notecardSentences = [];
+var sentenceEls = [];
+var firstSegEls = [];
+var cardEls = [];
+var prevSentenceIndexes = [];
 var numSentences = 0;
 
 (function () { // Will need to modify to handle multiple grafs
@@ -18,14 +20,14 @@ var numSentences = 0;
     
     if (el.tagName === 'SPAN') {
       
-      sentences.push(el);
-      firstSegs.push(el.firstElementChild); // IE9+
+      sentenceEls.push(el);
+      firstSegEls.push(el.firstElementChild); // IE9+
       numSentences++;
       
     } else {
       
-      notecards.push(el);
-      notecardSentences.push(numSentences - 1);
+      cardEls.push(el);
+      prevSentenceIndexes.push(numSentences - 1);
     }
     
     el = el.nextElementSibling; // IE9+
@@ -52,7 +54,7 @@ function hide(el) {
 }
 
 function getColumnLeft() {
-  return column.getBoundingClientRect().left;
+  return columnEl.getBoundingClientRect().left;
 }
 
 //
@@ -62,9 +64,49 @@ function getIndents() {
   var columnLeft = getColumnLeft();
   
   for (var i = 0; i < numSentences; i++) {
-    indents[i] = firstSegs[i].getClientRects()[0].left - columnLeft;
+    indents[i] = firstSegEls[i].getClientRects()[0].left - columnLeft;
   }
 }
+
+//
+
+function indent(sentenceIndex) {
+  
+  if (sentenceIndex < numSentences) {
+    sentenceEls[sentenceIndex].style.marginLeft = indents[sentenceIndex] + 'px';
+  }
+}
+
+function unindent(sentenceIndex) {
+  
+  if (sentenceIndex < numSentences) {
+    sentenceEls[sentenceIndex].style.marginLeft = '';
+  }
+}
+
+//
+
+function toggleCard(targetCardIndex) {
+  
+  if (currentCardIndex !== undefined) {
+    
+    hide(cardEls[currentCardIndex]);
+    unindent(prevSentenceIndexes[currentCardIndex] + 1);
+  }
+  
+  if (targetCardIndex !== undefined && targetCardIndex !== currentCardIndex) {
+    
+    show(cardEls[targetCardIndex]);
+    indent(prevSentenceIndexes[targetCardIndex] + 1);
+    currentCardIndex = targetCardIndex;
+    
+  } else {
+    
+    currentCardIndex = undefined;
+  }
+}
+
+// On page load or resize
 
 getIndents();
 
