2c01cd6be43485dacc463012b34bc79e32813efc
trppngl <jonathanconradmiller@gmail.com>
2018-01-26 21:48:17 -0500

    Testing click function

--- bastille.js
+++ bastille.js
@@ -1,10 +1,18 @@
 'use strict';
 
+var t0;
+var t1;
+var t2;
+var t3;
+
 var columnEl = document.getElementById('column');
+var topPaneEl = document.getElementById('top-pane');
 
 var currentCardIndex;
 
-var t0 = performance.now();
+t0 = performance.now();
+
+var backdropEls = document.getElementsByClassName('backdrop');
 
 var sentenceEls = [];
 var firstSegEls = [];
@@ -69,8 +77,8 @@ var numSentences = 0;
   }
 })();*/
 
-var t1 = performance.now();
-console.log((t1 - t0).toFixed(3) + "ms");
+t1 = performance.now();
+console.log('Populate arrays: ' + (t1 - t0).toFixed(3) + 'ms');
 
 var indents = new Array(numSentences);
 
@@ -85,21 +93,6 @@ function show(el) {
 function hide(el) {
   if (el) {
     el.classList.add('hide');
-  }
-}
-
-function getColumnLeft() {
-  return columnEl.getBoundingClientRect().left;
-}
-
-//
-
-function getIndents() {
-  
-  var columnLeft = getColumnLeft();
-  
-  for (var i = 0; i < numSentences; i++) {
-    indents[i] = firstSegEls[i].getClientRects()[0].left - columnLeft;
   }
 }
 
@@ -141,12 +134,114 @@ function toggleCard(targetCardIndex) {
   }
 }
 
+//
+
+function getOffsets() { // Separate functions for top and left?
+  
+  var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+  var scrollLeft = document.documentElement.scrollLeft || document.body.scrollLeft;
+  
+  var columnRect = columnEl.getBoundingClientRect();
+  
+  var top = columnRect.top - scrollTop;
+  var left = columnRect.left - scrollLeft;
+  
+  return {
+    top: top,
+    left: left
+  }
+}
+
+//
+
+function getIndentsResizeBackdrops() { // Combined two functions
+  
+  var offsets = getOffsets();
+  var rects = [];
+  
+  for (var i = 0; i < numSentences; i++) {
+    
+    indents[i] = firstSegEls[i].getClientRects()[0].left - offsets.left;
+    
+    rects[i] = sentenceEls[i].getBoundingClientRect();
+  }
+  
+  for (i = 0; i < numSentences; i++) {
+    
+    backdropEls[i].style.top = rects[i].top - offsets.top + 'px';
+    backdropEls[i].style.height = rects[i].height + 'px';
+  }
+}
+
+/*function getIndents() { // Close and reopen current card (if any)?
+  
+  var offsets = getOffsets();
+  
+  for (var i = 0; i < numSentences; i++) {
+    indents[i] = firstSegEls[i].getClientRects()[0].left - offsets.left;
+  }
+}*/
+
+/*function resizeBackdrops() {
+  
+  var offsets = getOffsets();
+  
+  var rects = [];
+  
+  var top;
+  var height;
+  
+  for (var i = 0; i < numSentences; i++) {
+    rects[i] = sentenceEls[i].getBoundingClientRect();
+  }
+  
+  for (i = 0; i < numSentences; i++) {
+    
+    top = rects[i].top - offsets.top;
+    height = rects[i].height;
+    
+    backdropEls[i].style.top = top + 'px';
+    backdropEls[i].style.height = height + 'px';
+  }
+}*/
+
 // On page load or resize
 
-getIndents();
+t0 = performance.now();
 
-/*
-var t0 = performance.now();
-var t1 = performance.now();
-console.log((t1 - t0).toFixed(3) + "ms");
-*/
+getIndentsResizeBackdrops();
+
+t1 = performance.now();
+console.log('getIndentsResizeBackdrops(): ' + (t1 - t0).toFixed(3) + 'ms');
+
+// Event handlers
+
+function handleClick(e) {
+
+  var xPos;
+  var yPos;
+  var element;
+  
+  hide(topPaneEl);
+  
+  xPos = e.clientX;
+  yPos = e.clientY;
+  element = document.elementFromPoint(xPos, yPos);
+  
+  show(topPaneEl);
+  
+  if (element.classList.contains('highlight')) {
+    element.classList.toggle('clicked');
+  }
+  
+  console.log('x: ' + xPos, 'y: ' + yPos);
+  console.log(element);
+}
+
+// Event listeners
+
+columnEl.addEventListener('click', handleClick, false);
+
+/*t0 = performance.now();
+t1 = performance.now();
+console.log((t1 - t0).toFixed(3) + 'ms');*/
